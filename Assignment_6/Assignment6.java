d9ba69c36ff273939de5bfedf1e3ab714b96d297
silver3456 <avseliverov@gmail.com>
2018-08-23 22:24:58 +0300

    Initial commit

---
+++ Assignment_6/Assignment6.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Assignment6 {
+    public static void main(String[] args) throws IOException {
+
+        int n, arr[];
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter array length: ");
+
+        n = in.nextInt();
+        arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = randomize(1, 10);
+
+        reverse(arr);
+    }
+
+    public static void reverse(int[] arr) {
+        for (int j = 0; j < arr.length; j++) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                int num = arr[i];
+                int nextNum = arr[i + 1];
+                if (num < nextNum) {
+                    int buf = arr[i];
+                    arr[i] = nextNum;
+                    arr[i + 1] = buf;
+                }
+            }
+        }
+        for (int i : arr)
+            System.out.print(i + " ");
+    }
+
+    public static int randomize(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+}
+
+
