d89bef5b2b1efa1e12c4ff67cb5d953f86dba44a
silver3456 <avseliverov@gmail.com>
2018-08-27 16:30:28 +0300

    Even numbers

---
+++ src/main/java/CodingChallange/EvenNumbers.java
@@ -0,0 +1,14 @@
+package CodingChallange;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int max = 10;
+        System.out.println("Even numbers are:");
+
+        for(int i=0; i<=max;i++){
+            if(i%2==0)
+                System.out.println(i);
+
+        }
+    }
+}
