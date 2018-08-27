8a32b876b9a947dd7faf153d1729224c3d413157
silver3456 <avseliverov@gmail.com>
2018-08-27 22:33:51 +0300

    Usage import BufferedReader, File, FileReader, ArrayList, Collections

---
+++ src/main/java/java_materials/SortSongs.java
@@ -0,0 +1,46 @@
+package java_materials;
+
+import CodingChallange.Training;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortSongs {
+    ArrayList<String> songList = new ArrayList<String>();
+
+    public static void main(String[] args) {
+
+        new Training().go();
+    }
+
+    public void go() {
+        getSongs();
+        System.out.println(songList);
+        Collections.sort(songList);
+        System.out.println(songList);
+    }
+
+    void getSongs() {
+        try {
+            File file = new File("/Users/alexander/Desktop/SongsList.txt");
+            //  File a = new File("/usr/local/bin/geeks");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void addSong(String lineToParse) {
+        String[] tokens = lineToParse.split("/");
+        songList.add(tokens[0]);
+
+    }
+}
