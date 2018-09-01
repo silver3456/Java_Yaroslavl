dbf3f364d9446442d83de4428ac3516b2e1b0d7f
silver3456 <avseliverov@gmail.com>
2018-09-01 18:36:27 +0300

    Updated changes

--- src/main/java/_26_08/Assignment_1/CaesarCipherSimple.java
+++ src/main/java/_26_08/Assignment_1/CaesarCipherSimple.java
@@ -3,11 +3,22 @@ package _26_08.Assignment_1;
 public class CaesarCipherSimple {
     public static void main(String[] args) {
 
-        String str = "Let's go places";
 
-        System.out.println("Encrypted message is: " + CaesarCipherSimple.encode(str, 3));
+        //Option one
+//        String str = "cqwrqlr";
+//        System.out.println("Encrypted message is: " + CaesarCipherSimple.encode(str, 3));
+//
+//        System.out.println("Decrypted message is: " + CaesarCipherSimple.decode(CaesarCipherSimple.encode(str, 3), 3));
 
-        System.out.println("Decrypted message is: " + CaesarCipherSimple.decode(CaesarCipherSimple.encode(str, 3), 3));
+        
+        //Option two
+        String encMessage = encode("Antonio let is go fuck busty girls", 3);
+        System.out.println(encMessage);
+
+        String decMessage = decode("Dqwrqlr ohw lv jr ixfn exvwb jluov", 3);
+        System.out.println(decMessage);
+
+
     }
 
     public static String decode(String enc, int offset) {
