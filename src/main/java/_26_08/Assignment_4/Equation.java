16f50eca95c50960c8ec56cc2c9b7e414c62e82c
silver3456 <avseliverov@gmail.com>
2018-09-02 14:59:32 +0300

    Added full equation solution

--- src/main/java/_26_08/Assignment_4/Equation.java
+++ src/main/java/_26_08/Assignment_4/Equation.java
@@ -6,7 +6,7 @@ public class Equation {
     public static void main(String[] args) {
         double a, b, c;
 
-        Scanner sc  = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Input a");
         a = sc.nextDouble();
@@ -16,5 +16,31 @@ public class Equation {
 
         System.out.println("Input c");
         c = sc.nextDouble();
+
+        System.out.println(a + "x^2 + " + b + "x + " + c + " = 0");
+
+        double disc = b * b - 4 * a * c;
+
+        if (disc < 0) {
+            System.out.println("Doesn't have a root");
+        } else if (disc == 0) {
+            double root = calcValue(a, b, disc, true);
+            System.out.println("Root is " + root);
+        } else {
+            double root1 = calcValue(a, b, disc, false);
+            double root2 = calcValue(a, b, disc, true);
+
+            System.out.println("Root 1 = " + root1 + ", Root 2 = " + root2);
+        }
+
+    }
+
+    public static double calcValue(double a, double b, double disc,
+                                   boolean isPlus) {
+        int coef = 1;
+        if (!isPlus) {
+            coef = -1;
+        }
+        return (-b + coef * Math.sqrt(disc)) / (2 * a);
     }
 }
