6e90ca858bd2ce208f4886822c9100a66c8fc1a6
silver3456 <avseliverov@gmail.com>
2018-09-04 16:40:00 +0300

    Added encoded method

---
+++ src/main/java/_26_08/Assignment_1/CaserCipherV3.java
@@ -0,0 +1,27 @@
+package _26_08.Assignment_1;
+
+public class CaserCipherV3 {
+
+    public static void main(String[] args) {
+
+        String encoded = encode("help", 3);
+        System.out.println(encoded);
+    }
+
+    public static String encode(String enc, int key) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        char shifted = 0;
+        for (char i : enc.toCharArray()) {
+            int askii = (int) i + key;
+            shifted = (char) askii;
+            String var = String.valueOf(shifted);
+            StringBuilder encoded = stringBuilder.append(var);
+
+        }
+        return stringBuilder.toString();
+    }
+}
+
+
+
