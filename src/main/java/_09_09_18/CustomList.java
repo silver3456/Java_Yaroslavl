73bf9474f84c3c8c53eecd701aa49ee27a48f214
silver3456 <avseliverov@gmail.com>
2018-09-09 11:49:06 +0300

    Created analogue of ArrayList and added tests

---
+++ src/main/java/_09_09_18/CustomList.java
@@ -0,0 +1,12 @@
+package _09_09_18;
+
+public interface CustomList {
+    int get(int index);
+
+    void set(int index, int number);
+
+    void add(int number);
+
+    int size();
+
+}
---
+++ src/test/java/CustomLIstTest.java
@@ -0,0 +1,27 @@
+import _09_09_18.CustomList;
+import _09_09_18.CustomListImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CustomLIstTest {
+
+    @Test
+    public void testList() {
+        CustomList list = new CustomListImpl();
+        list.add(1);
+        System.out.println(list.get(0));
+        Assert.assertEquals(1, list.size());
+
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        list.set(1, 55);
+        Assert.assertEquals(55, list.get(1));
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+
+        }
+        Assert.assertEquals(11, list.size());
+    }
+}
