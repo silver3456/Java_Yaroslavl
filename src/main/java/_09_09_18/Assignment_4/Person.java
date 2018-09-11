a34e9f1b4fa7903ebce53684c4b72a1f26ba6ed7
silver3456 <avseliverov@gmail.com>
2018-09-11 18:41:12 +0300

    Added procedural paradigm for finding a person with min age

---
+++ src/main/java/_09_09_18/Assignment_4/Person.java
@@ -0,0 +1,63 @@
+package _09_09_18.Assignment_4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+    static String firstName;
+    static String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int withAge() {
+        return age;
+    }
+
+    public static void main(String[] args) {
+
+
+        Person personOne = new Person("Михаил ", "Иванов");
+        personOne.setAge(17);
+
+        Person personTwo = new Person("Евгений ", "Тетерев");
+        personTwo.setAge(20);
+
+        Person personThree = new Person("Алексей ", "Федоров");
+        personThree.setAge(15);
+
+        Person personFour = new Person("Марина ", "Олексенко");
+        personFour.setAge(13);
+
+        Person personFive = new Person("Виктория", "Кравцова");
+        personFive.setAge(25);
+
+
+        List<Person> person = new ArrayList<>();
+        person.add(personOne);
+        person.add(personTwo);
+        person.add(personThree);
+        person.add(personFour);
+        person.add(personFive);
+
+
+        int min = person.get(0).withAge();
+
+        for (int i = 1; i < person.size(); i++) {
+            if (min > person.get(i).withAge()) {
+                min = person.get(i).withAge();
+            }
+
+        }
+        System.out.print(firstName + " " + lastName + " age " + min);
+    }
+}
+
