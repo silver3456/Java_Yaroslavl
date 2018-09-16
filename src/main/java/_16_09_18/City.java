968a891f59b0fd537020db1618fbb3da0468d86c
silver3456 <avseliverov@gmail.com>
2018-09-16 15:51:39 +0300

    Added City, sorting, lambda and output

---
+++ src/main/java/_16_09_18/City.java
@@ -0,0 +1,36 @@
+package _16_09_18;
+
+public class City {
+    private String name;
+    private int citizens;
+
+    public City(String name, int citizens) {
+
+        setCitizens(citizens);
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCitizens() {
+        return citizens;
+    }
+
+    public void setCitizens(int citizens) {
+        this.citizens = citizens;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", citizens=" + citizens +
+                '}';
+    }
+}
