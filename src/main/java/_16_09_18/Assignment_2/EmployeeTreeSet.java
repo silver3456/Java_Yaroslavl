6b26f7b6a9d390f444c6c575f88bcbc9420b114b
silver3456 <avseliverov@gmail.com>
2018-09-21 20:44:26 +0300

    Added solution using TreeSet constructor with Comparator<?> parameter

--- src/main/java/_16_09_18/Assignment_2/EmployeeTreeSet.java
+++ src/main/java/_16_09_18/Assignment_2/EmployeeTreeSet.java
@@ -1,4 +1,67 @@
 package _16_09_18.Assignment_2;
 
+import java.util.Comparator;
+import java.util.Objects;
+
 public class EmployeeTreeSet {
+
+    private String lastName;
+    private int salary;
+
+    public EmployeeTreeSet(String lastName, int salary) {
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public static class MyComparator implements Comparator<EmployeeTreeSet> {
+
+        @Override
+        public int compare(EmployeeTreeSet o1, EmployeeTreeSet o2) {
+            if (o1.getSalary() > o2.getSalary())
+                return -1;
+            else if (o1.getSalary() < o2.getSalary())
+                return 1;
+
+            else return 0;
+        }
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EmployeeTreeSet)) return false;
+        EmployeeTreeSet that = (EmployeeTreeSet) o;
+        return salary == that.salary &&
+                Objects.equals(lastName, that.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lastName, salary);
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeTreeSet{" +
+                "lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
--- src/main/java/_16_09_18/Assignment_2/EmployeeTreeSetTest.java
+++ src/main/java/_16_09_18/Assignment_2/EmployeeTreeSetTest.java
@@ -1,4 +1,20 @@
 package _16_09_18.Assignment_2;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class EmployeeTreeSetTest {
+    public static void main(String[] args) {
+
+        SortedSet<EmployeeTreeSet> employees = new TreeSet<>(new EmployeeTreeSet.MyComparator());
+        employees.add(new EmployeeTreeSet("Egorov", 10000));
+        employees.add(new EmployeeTreeSet("Prohorov", 15000));
+        employees.add(new EmployeeTreeSet("Saraev", 9000));
+        employees.add(new EmployeeTreeSet("Pukhov", 13000));
+        employees.add(new EmployeeTreeSet("Medvedev", 20000));
+
+        for (EmployeeTreeSet emp : employees)
+            System.out.println(emp + " ");
+
+    }
 }
