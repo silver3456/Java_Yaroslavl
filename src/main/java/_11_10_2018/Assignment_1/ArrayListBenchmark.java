a308a787c02f2c131729beb3e814da3089ed2143
silver3456 <avseliverov@gmail.com>
2018-10-11 17:46:39 +0300

    ArrayListBenchmarking

---
+++ src/main/java/_11_10_2018/Assignment_1/ArrayListBenchmark.java
@@ -0,0 +1,44 @@
+package _11_10_2018.Assignment_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListBenchmark {
+    public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<>();
+        long startTime = System.nanoTime();
+
+        //add
+
+        for (int i = 0; i < 100000; i++) {
+            arrayList.add(i);
+        }
+
+        long endtime = System.nanoTime();
+        long duration = endtime - startTime;
+        System.out.println("ArrayList add: \t" + duration);
+
+
+        startTime = System.nanoTime();
+
+        //get
+
+        for (int i = 0; i < 100000; i++) {
+            arrayList.get(i);
+        }
+
+        endtime = System.nanoTime();
+        duration = endtime - startTime;
+        System.out.println("ArrayList get: \t" + duration);
+
+        startTime = System.nanoTime();
+
+        for (int i = 99999; i >= 0; i--) {
+            arrayList.remove(i);
+        }
+        endtime = System.nanoTime();
+        duration = endtime - startTime;
+        System.out.println("ArrayList remove: \t" + duration);
+
+    }
+}
