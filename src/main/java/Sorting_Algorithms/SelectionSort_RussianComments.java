0029e0b996503f1b828b826c7692db349aaabdfe
silver3456 <avseliverov@gmail.com>
2018-10-15 12:24:48 +0300

    Modified Selection Sort

---
+++ src/main/java/Sorting_Algorithms/SelectionSort_RussianComments.java
@@ -0,0 +1,47 @@
+package Sorting_Algorithms;
+
+public class SelectionSort_RussianComments {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 10);
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        selectionSort(arr);
+
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+
+    }
+
+    public static void selectionSort(int[] arr) {
+    /*По очереди будем просматривать все подмножества
+      элементов массива (0 - последний, 1-последний,
+      2-последний,...)*/
+        for (int i = 0; i < arr.length; i++) {
+        /*Предполагаем, что первый элемент (в каждом
+           подмножестве элементов) является минимальным */
+            int min = arr[i];
+            int min_i = i;
+        /*В оставшейся части подмножества ищем элемент,
+           который меньше предположенного минимума*/
+            for (int j = i + 1; j < arr.length; j++) {
+                //Если находим, запоминаем его индекс
+                if (arr[j] < min) {
+                    min = arr[j];
+                    min_i = j;
+                }
+            }
+        /*Если нашелся элемент, меньший, чем на текущей позиции,
+          меняем их местами*/
+            if (i != min_i) {
+                int tmp = arr[i];
+                arr[i] = arr[min_i];
+                arr[min_i] = tmp;
+            }
+        }
+    }
+}
