d7ca6e3e5a8def2a62157bc17c0f10ba5aa4e76c
silver3456 <avseliverov@gmail.com>
2018-10-19 12:59:00 +0300

    Full solution count Tigers

---
+++ src/main/java/JavaRush/Assignment_1/Assignment_2/Solution.java
@@ -0,0 +1,14 @@
+package JavaRush.Assignment_1.Assignment_2;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        for (int i = 0; i < 3; i++) {
+            new Tiger();
+            count += 1;
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/main/java/JavaRush/Assignment_1/Assignment_2/Tiger.java
@@ -0,0 +1,8 @@
+package JavaRush.Assignment_1.Assignment_2;
+
+public class Tiger {
+
+    public Tiger() {
+
+    }
+}
