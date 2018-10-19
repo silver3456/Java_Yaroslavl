7566a70f424244264f76248c24daedf068c235d1
silver3456 <avseliverov@gmail.com>
2018-10-19 18:35:14 +0300

    Print triangle

---
+++ src/main/java/CodingChallange/Triangle.java
@@ -0,0 +1,13 @@
+package CodingChallange;
+
+public class Triangle {
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            for (int j = 0; j < i; j++)
+                System.out.print("8");
+            System.out.println();
+        }
+
+    }
+}
+
