b6c035997bf8e5717d33b210e66e93fb9f38a9de
silver3456 <avseliverov@gmail.com>
2018-10-19 18:43:43 +0300

    Print Lines

---
+++ src/main/java/JavaRush/Assignment_1/Assignment_6/PrintLines.java
@@ -0,0 +1,14 @@
+package JavaRush.Assignment_1.Assignment_6;
+
+public class PrintLines {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("8");
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("8");
+        }
+    }
+}
