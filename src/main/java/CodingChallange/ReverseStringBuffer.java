d0bfd3413444fda9f9f9760f3750cc035e4a4838
silver3456 <avseliverov@gmail.com>
2018-10-20 18:26:44 +0300

    ReverseStringBuffer

---
+++ src/main/java/CodingChallange/ReverseStringBuffer.java
@@ -0,0 +1,17 @@
+package CodingChallange;
+
+
+public class ReverseStringBuffer {
+    public static void main(String[] args) {
+
+        int number = 12345;
+        int original = number;
+
+        StringBuffer stringBuffer = new StringBuffer(String.valueOf(number));
+        stringBuffer.reverse();
+        int reverse = Integer.parseInt(stringBuffer.toString());
+        System.out.println(original + " : " + reverse);
+
+
+    }
+}
