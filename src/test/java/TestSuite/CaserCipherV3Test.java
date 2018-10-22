65e8db71ca6d86ed52214d66534c28db0d9bab1f
silver3456 <avseliverov@gmail.com>
2018-10-22 17:11:55 +0300

    Changed name

--- src/test/java/TestSuite/CaserCipherV3Test.java
+++ src/test/java/TestSuite/CaserCipherV3Test.java
@@ -1,6 +1,6 @@
 package TestSuite;
 
-import _26_08.Assignment_1.CaserCipherV3;
+import _26_08.Assignment_1.CaeserCipherV3;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,7 +8,7 @@ public class CaserCipherV3Test {
 
     @Test
     public void testCaserCipher(){
-        String result = CaserCipherV3.encode("hello", 3);
+        String result = CaeserCipherV3.encode("hello", 3);
         Assert.assertEquals(result, "khoor");
 
     }
