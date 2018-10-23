39f42815c7a346b9dc755a41c279b700bd9f20a8
silver3456 <avseliverov@gmail.com>
2018-10-23 19:05:17 +0300

    Added descriptions to tests

--- src/test/java/_21_10_18/VKTest.java
+++ src/test/java/_21_10_18/VKTest.java
@@ -6,6 +6,7 @@ import _26_08.Assignment_1.CaeserCipherV3;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -22,23 +23,31 @@ public class VKTest extends TestBase {
         lp = new LoginPage(driver);
     }
 
+    //Verify ability to login
     @Test
     public void testLogin() {
         lp.login(CaeserCipherV3.decode("pfisbo+_bpq=j^fi+or", -3),
                 CaeserCipherV3.decode("Pfisbo=.5./", -3));
         String title_actual = driver.getTitle();
         Assert.assertEquals("Welcome! | VK", title_actual);
+
     }
 
+    //Verify if user has undread messages
     @Test
     public void testUnreadMessages() {
         boolean verifymMessage = driver.findElement(By.xpath("//*[@id=\"l_msg\"]/a/span/span[1]/span")).getText().isEmpty();
         org.testng.Assert.assertFalse(verifymMessage);
     }
 
+    // Verify number of friends
     @Test
     public void verifyNumOfFriends() {
-
+        WebElement friends = driver.findElement(By.cssSelector(" #l_fr > a > span > span.left_label.inl_bl"));
+        friends.click();
+        WebElement friendsNum = driver.findElement(By.cssSelector("#friends_tab_all > a > span"));
+        int sizeFriends = Integer.parseInt(friendsNum.getText());
+        Assert.assertEquals(257, sizeFriends);
     }
 }
 
