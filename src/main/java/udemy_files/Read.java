3839cb41ee5413c55df1913f6b07c817a358b424
silver3456 <avseliverov@gmail.com>
2019-01-24 20:40:47 -0800

    Added a class to read the file

---
+++ src/main/java/udemy_files/Read.java
@@ -0,0 +1,39 @@
+package udemy_files;
+
+import java.io.*;
+
+public class Read {
+
+
+    public static void main(String[] args) {
+        //1.Define the file(path) that I want to read
+
+        String filename = "/Users/alexander/SDET_Files/FileToRead.txt";
+        String text = null;
+
+        //2.Create the file in Java
+        File file = new File(filename);
+
+
+        try {
+            //3.Open the file
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            //4.Read the file
+            text = br.readLine();
+
+            //5.Close the resource
+            br.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: File not found: " + filename);
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("ERROR: Could not read the data: " + filename);
+            e.printStackTrace();
+        } finally {
+            System.out.println("Finished reading the file");
+        }
+        System.out.println(text);
+    }
+}
