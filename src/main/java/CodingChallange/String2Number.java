a356f98856d8750775c00362808fc7540f0ce8a3
silver3456 <avseliverov@gmail.com>
2019-02-06 21:40:55 -0800

    Convert string to integer

---
+++ src/main/java/CodingChallange/String2Number.java
@@ -0,0 +1,14 @@
+package CodingChallange;
+
+//given the string "1234" the result should be the number 1234
+
+public class String2Number {
+    public static void main(String[] args) {
+
+        String word = "12345";
+
+        int nums = Integer.parseInt(word);
+        System.out.println(nums);
+    }
+}
+
