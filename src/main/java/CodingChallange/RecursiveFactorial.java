10f10647cb276a6822f01aad5f3b2ab90d5e77df
silver3456 <avseliverov@gmail.com>
2019-03-05 21:44:26 -0800

    change if condition to ==1

--- src/main/java/CodingChallange/RecursiveFactorial.java
+++ src/main/java/CodingChallange/RecursiveFactorial.java
@@ -4,15 +4,16 @@ package CodingChallange;
 
 public class RecursiveFactorial {
     public static long factorial(int number) {
-        if (number < 1) {
+        if (number == 1) {
             return 1; // Base case
         } else {
             return number * factorial(number - 1);
         }
+
     }
 
     public static void main(String[] args) {
-        int num = 5;
+        int num = 4;
         System.out.println(factorial(num));
     }
 }
