cd4da826f0a3d68c3d76e5a1d4c487b6d568a42b
silver3456 <avseliverov@gmail.com>
2019-04-11 10:31:30 -0700

    Add HashSet methods example

---
+++ src/main/java/data_structures/Set/HashSetIterator.java
@@ -0,0 +1,38 @@
+package data_structures.Set;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetIterator {
+    public static void main(String args[]) {
+        // Creating an empty HashSet
+        HashSet<String> set = new HashSet<String>();
+
+        // Use add() method to add elements into the Set
+        set.add("Welcome");
+        set.add("To");
+        set.add("Geeks");
+        set.add("4");
+        set.add("Geeks");
+
+        // Displaying the HashSet
+        System.out.println("HashSet: " + set);
+
+        // Creating an iterator
+        Iterator value = set.iterator();
+
+
+        // Displaying the values after iterating through the set
+        System.out.println("The iterator values are: ");
+        while (value.hasNext()) {
+
+            //Remove element during iteration
+            if (value.next().equals("4")) {
+                value.remove();
+                System.out.println("After element removal" + set);
+            }
+
+        }
+
+    }
+}
