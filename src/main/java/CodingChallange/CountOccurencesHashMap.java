b47fbcf6e6c9dbf3088d3900325522557190c381
silver3456 <avseliverov@gmail.com>
2020-02-11 15:51:15 -0800

    Simplify solution

--- src/main/java/CodingChallange/CountOccurencesHashMap.java
+++ src/main/java/CodingChallange/CountOccurencesHashMap.java
@@ -11,19 +11,23 @@ public class CountOccurencesHashMap {
 
 
         Map<String, Integer> map = new HashMap<>();
-        int count;
+        int count = 1;
 
         for (String word : wordsArray) {
             if (map.containsKey(word)) {
-                count = map.get(word);
                 map.put(word, count + 1);
             } else {
-                map.put(word, 1);
+                map.put(word, count);
             }
         }
 
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue().toString());
+//         for (Map.Entry<String, Integer> entry : map.entrySet()) {
+//             System.out.println(entry.getKey() + ": " + entry.getValue().toString());
+//         }
+// OR
+       
+        for (Map.Entry entry : map.entrySet()) {
+            System.out.println(entry);
         }
     }
 }
