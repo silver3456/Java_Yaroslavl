789b75af45197875dafa5cb62e05f5d9f565934d
silver3456 <avseliverov@gmail.com>
2020-03-25 14:10:37 -0700

    Set up mobile emulator environment

---
+++ src/main/java/java_materials/MobileEmulatorSetUp.java
@@ -0,0 +1,18 @@
+public WebDriver setMobileWebDriver() throws MalformedURLException {
+        Map<String, Object> deviceMetrics = new HashMap<>();
+        deviceMetrics.put("width", 360);
+        deviceMetrics.put("height", 640);
+        deviceMetrics.put("pixelRatio", 3.0);
+        Map<String, Object> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceMetrics", deviceMetrics);
+        mobileEmulation.put("userAgent", "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19");
+
+        Map<String, Object> chromeOptions = new HashMap<>();
+        chromeOptions.put("mobileEmulation", mobileEmulation);
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        capabilities.setCapability(CapabilityType.PROXY, seleniumProxy); // configure it as a desired capability
+
+        return driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
